5095f0490118dbcb796e155e179f3a2e8c141e4e
Orme <spamoremas@gmail.com>
2026-06-07 20:35:54 +0200

    refactor(routes): Update app routing paths and component imports
    *   Updates import statements in `app.routes.ts` to use correct page module paths.
    *   Simplifies route definitions in `app.routes.ts` by removing explicit path/component wrapping for cleaner structure.

--- Frontend/Web/src/app/app.routes.ts
+++ Frontend/Web/src/app/app.routes.ts
@@ -1,30 +1,16 @@
 import {Routes} from '@angular/router';
-import {MainPage} from './pages/main-page/main.page';
-import {ClothingManagementPage} from './pages/clothing-management-page/clothing-management.page';
-import {TestBedPage} from './pages/test-bed-page/test-bed.page';
 import {developmentToggleGuard} from './guards/development-toggle-guard';
-import {OutfitEditPage} from './pages/outfit-preview-page/outfit-edit.page';
+import {MainPage} from './pages/main.page/main.page';
+import {OutfitEditPage} from './pages/outfit-preview.page/outfit-edit.page';
+import {ClothingManagementPage} from './pages/clothing-management.page/clothing-management.page';
+import {TestBedPage} from './pages/test-bed.page/test-bed.page';
+import {LoginPage} from './pages/login.page/login.page';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: MainPage
-  },
-  {
-    path: 'add',
-    component: OutfitEditPage
-  },
-  {
-    path: 'clothing',
-    component: ClothingManagementPage
-  },
-  {
-    path: 'outfit/:id',
-    component: OutfitEditPage
-  },
-  {
-    path: 'testbed',
-    component: TestBedPage,
-    canMatch: [developmentToggleGuard]
-  }
+  {path: '', component: MainPage},
+  {path: 'login', component: LoginPage},
+  {path: 'add', component: OutfitEditPage},
+  {path: 'clothing', component: ClothingManagementPage},
+  {path: 'outfit/:id', component: OutfitEditPage},
+  {path: 'testbed', component: TestBedPage, canMatch: [developmentToggleGuard]}
 ];
--- Frontend/Web/src/app/components/icons/sun-icon/sun.icon.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'fp-sun-icon',
-  imports: [],
-  templateUrl: './sun.icon.html',
-  styleUrl: './sun.icon.scss',
-})
-export class SunIcon {}
